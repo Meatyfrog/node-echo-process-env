148a7ce0164f6573d22eab0799815d31e378c90d
Chris Chafin <speedsolar@gmail.com>
2018-02-28 08:01:03 -0500

    initial postgres implementation

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log(process.env);
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.databasename, process.env.databaseuser, process.env.databasepassword, {
+  host: process.env.ROUTINATOR_DB_PORT,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
